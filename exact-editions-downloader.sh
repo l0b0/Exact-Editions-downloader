cd27b86fa8ee2dce49131a6d2292098604c4b0a3
Victor Engmark <victor.engmark@gmail.com>
2016-06-05 16:14:05 +0100

    Get a sensible name for each issue

--- exact-editions-downloader.sh
+++ exact-editions-downloader.sh
@@ -31,8 +31,19 @@ wget --keep-session-cookies --output-document=/dev/null --post-data="username=${
 issues_file="issues.html"
 wget --load-cookies="${cookie_file}" --max-redirect=0 --output-document="${issues_file}" "https://reader.exacteditions.com/magazines/${magazine}/issues"
 
-for issue_path in $(grep --only-matching --perl-regexp '(?<=href="/)issues/[^/"]+' "${issues_file}")
+while IFS= read -r -u 3 line
 do
+    issue_path="$(grep --only-matching --perl-regexp '(?<=href="/)issues/[^/"]+' <<< "$line")"
+    publication_date="$(grep --only-matching --perl-regexp '(?<=data-publication-date=")[^"]+' <<< "$line")"
+    name="$(grep --only-matching --perl-regexp '(?<=data-name=")[^"]+' <<< "$line" | sed 's#\( \)\?/#,#g')"
+    issue_pdf_path="${publication_date} ${name}.pdf"
+
+    # Skip if already done
+    if [ -e "${issue_pdf_path}" ]
+    then
+        continue
+    fi
+
     spread_directory="${issue_path}/spread"
     mkdir --parents "${spread_directory}"
 
@@ -40,7 +51,7 @@ do
     thumbs_file="${issue_path}/thumbs.html"
     wget --load-cookies="${cookie_file}" --max-redirect=0 --output-document="${thumbs_file}" "https://reader.exacteditions.com/${issue_path}/thumbs"
 
-    for spread_path in $(grep --only-matching --perl-regexp '(?<=href="/)issues/[^/"]+/spread/[^/"]+' "${thumbs_file}")
+    while read -r -u 4 spread_path
     do
         # Get spreads
         spread_pdf_path="${spread_path}.pdf"
@@ -50,12 +61,8 @@ do
             wget --load-cookies="${cookie_file}" --max-redirect=0 --output-document="${local_spread_pdf_path}" "https://reader.exacteditions.com/${spread_pdf_path}"
             sleep 10
         fi
-    done
+    done 4< <(grep --only-matching --perl-regexp '(?<=href="/)issues/[^/"]+/spread/[^/"]+' "${thumbs_file}")
 
     # Convert to single PDF file
-    local_issue_pdf_path="${issue_path}/issue.pdf"
-    if [ ! -e "${local_issue_pdf_path}" ]
-    then
-        pdfunite $(find "${spread_directory}" -mindepth 1 | sort --field-separator=/ --key=4 --numeric-sort) "${local_issue_pdf_path}"
-    fi
-done
+    pdfunite $(find "${spread_directory}" -mindepth 1 | sort --field-separator=/ --key=4 --numeric-sort) "${issue_pdf_path}"
+done 3< <(grep 'href="/issues/[^/"]\+' "${issues_file}")
